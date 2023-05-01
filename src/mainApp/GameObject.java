65042777b0d833085d6397593072625e512c5fb3
hansenkm <hansenkm@137.112.199.81>
2023-05-01 14:21:17 -0400

    fixed error we cause to learn the errors

--- src/mainApp/GameObject.java
+++ src/mainApp/GameObject.java
@@ -3,13 +3,7 @@ package mainApp;
 public class GameObject {
 
 	public GameObject() {
-<<<<<<< HEAD
-		// TODO Auto-generated constructor stub
-		System.out.println("fsa");
-=======
-		System.out.println("YAYAYAY");
-		System.out.println("what");
->>>>>>> branch 'master' of https://github.com/rhit-csse220/csse220-spring-2023-final-project-s23_a303.git
+
 	}
 
 }
