a733a630baf6ba202e1b568b9fb61b71e8387db7
Kallen Selby <crazykalo27@gmail.com>
2023-05-02 01:28:13 -0400

    deleted unused imports

--- src/mainApp/FileReader.java
+++ src/mainApp/FileReader.java
@@ -1,9 +1,7 @@
 package mainApp;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
