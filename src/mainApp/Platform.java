a50ad7401e0215915e4aed92afea0117b83f008d
hansenkm <hansenkm@137.112.199.81>
2023-05-03 18:19:14 -0400

    added drawOn to platform class

--- src/mainApp/Platform.java
+++ src/mainApp/Platform.java
@@ -1,9 +1,18 @@
 package mainApp;
 
-public class Platform extends GameObject {
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 
+public class Platform extends GameObject {
+	private final int WIDTH = 100;
+	private final int HEIGHT = 100;
 	public Platform(double xCord,double yCord) {
 		super(xCord, yCord);
 	}
-
+	public void drawOn(Graphics2D g2) {
+		g2.translate(this.getXCent(), this.getYCent());
+		g2.fill(new Rectangle2D.Double(-WIDTH/2,-HEIGHT/2,WIDTH,HEIGHT));
+		g2.translate(-this.getXCent(), -this.getYCent());
+	}
+	
 }
