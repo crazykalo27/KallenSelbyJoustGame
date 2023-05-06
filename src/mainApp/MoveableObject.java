137dd68eae381edb2783f629f9d3a140aaf61bfb
yangnj <yangnj@rose-hulman.edu>
2023-05-05 22:41:25 -0400

    Nevermind shouldn't have pushed that

--- src/mainApp/MoveableObject.java
+++ src/mainApp/MoveableObject.java
@@ -25,11 +25,11 @@ public class MoveableObject extends GameObject {
 		this.move(xVelocity, yVelocity);
 		
 		if (this.hasGravity) {
-			this.move(0, GRAVITY_STRENGTH);
+			this.yVelocity += GRAVITY_STRENGTH;
 		}
 		
 		if (this.hasFriction) {
-			this.move(-Math.signum(xVelocity) * FRICTION_STRENGTH, 0);
+			this.addXVelocity(-Math.signum(xVelocity) * FRICTION_STRENGTH);
 		}
 	}
 
