edb7e686c97145042a56b473e39f4e66b2a7f0ae
Kallen Selby <crazykalo27@gmail.com>
2023-05-08 15:51:54 -0400

    added some random oscillating movement to the green baddie class

--- src/mainApp/Baddie.java
+++ src/mainApp/Baddie.java
@@ -1,36 +1,55 @@
 package mainApp;
+
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
+import java.util.Random;
 
 //todo add collionsons and gravity 
 public class Baddie extends MoveableObject {
+	Random r = new Random();
 	private final double DIST = 50;
 	private final Color COL = Color.green;
-	//private static final int WIDTH = 100;
-	//private static final int HEIGHT = 100;
+	// private static final int WIDTH = 100;
+	// private static final int HEIGHT = 100;
 	private double speed;
-	
+	private int ticks;
+	private int waitNum;
 
 	public Baddie(int xCent, int yCent, double speed) {
-		super(xCent,yCent);
+		super(xCent, yCent);
 		this.speed = speed;
 		this.setHasGravity(false);
+		this.ticks = 0;
+		this.waitNum = r.nextInt(60, 160);
+
 	}
+
 	public void drawOn(Graphics2D g2) {
 		g2.setColor(COL);
 		g2.translate(this.getXCent(), this.getYCent());
-		g2.fill(new Rectangle2D.Double(-this.getWidth()/2,-this.getHeight()/2,this.getWidth(),this.getHeight()));
+		g2.fill(new Rectangle2D.Double(-this.getWidth() / 2, -this.getHeight() / 2, this.getWidth(), this.getHeight()));
 		g2.translate(-this.getXCent(), -this.getYCent());
 	}
-	
+
 	@Override
 	public void update() {
-		double i = Math.random();
-		if(i>=.6) {
-			this.speed = -this.speed;
+		this.setxVelocity(Math.pow(speed, 2) * Math.signum(speed));
+
+		ticks++;
+
+		if (ticks == waitNum) {
+			int diry = -1;
+			if(r.nextBoolean()) {
+				diry *= -1;
+			}
+			this.setyVelocity(speed * diry);
+			System.out.println("updated " + waitNum);
+			speed *= -1;
+			ticks = 0;
+			waitNum = r.nextInt(60, 160);
 		}
-		this.setXVelocity(this.getXVelocity()+this.speed);
+
 		super.update();
 	}
 }
