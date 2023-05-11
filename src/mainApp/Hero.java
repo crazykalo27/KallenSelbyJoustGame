353688e8ae2f15176ae2505272942edb37be7d76
yangnj <yangnj@rose-hulman.edu>
2023-05-11 13:52:54 -0400

    Fixed odd corner behavior on hero colisions

--- src/mainApp/Hero.java
+++ src/mainApp/Hero.java
@@ -91,7 +91,9 @@ public class Hero extends MoveableObject {
 		double otherh = overlap.getHeight();
 		double otherw = overlap.getWidth();
 		if(otherh>=otherw) {
-			this.move(-Math.signum(this.getXVelocity())*otherw, 0);
+			// TODO: Fix getting caught on ceiling
+			int direction = (int) Math.signum(other.getXCent() - this.getXCent());
+			this.move(-direction*otherw, 0);
 			this.setXVelocity(0);
 		}else {
 			this.move(0, -Math.signum(this.getYVelocity())*otherh);
