5475e33f14fab55656aac27db8fbe2b9c7694853
Kallen Selby <crazykalo27@gmail.com>
2023-05-15 13:29:42 -0400

    Changed platform so it can have a lava mode
    Merge branch 'master' of
    https://github.com/rhit-csse220/csse220-spring-2023-final-project-s23_a303.git

--- src/mainApp/Platform.java
+++ src/mainApp/Platform.java
@@ -12,19 +12,26 @@ import java.awt.geom.Rectangle2D;
  */
 public class Platform extends GameObject {
 
-	private final Color COLOR = Color.ORANGE;
+	private final Color DEFAULT_COLOR = Color.ORANGE;
+	private final Color LAVA_COLOR = Color.RED;
 	private double WIDTH;
 	private double HEIGHT;
+	private boolean isLava;
 	
-	public Platform(double xCord,double yCord) {
+	public Platform(double xCord,double yCord, boolean isLava) {
 		super(xCord, yCord);
 		WIDTH = this.getWidth();
 		HEIGHT = this.getHeight();
+		this.isLava = isLava;
 	}
 	
 	public void drawOn(Graphics2D g2) {
 		g2.translate(this.getXCent(), this.getYCent());
-		g2.setColor(this.COLOR);
+		Color temp = DEFAULT_COLOR;
+		if(isLava) {
+			temp = LAVA_COLOR;
+		}
+		g2.setColor(temp);
 		//g2.translate(this.getXCent(), this.getYCent());
 		g2.fill(new Rectangle2D.Double(-WIDTH/2,-HEIGHT/2,WIDTH,HEIGHT));
 		g2.setColor(Color.BLACK);
@@ -32,6 +39,38 @@ public class Platform extends GameObject {
 		
 		g2.translate(-this.getXCent(), -this.getYCent());
 		
+	}
+
+	public double getWIDTH() {
+		return WIDTH;
+	}
+
+	public void setWIDTH(double wIDTH) {
+		WIDTH = wIDTH;
+	}
+
+	public double getHEIGHT() {
+		return HEIGHT;
+	}
+
+	public void setHEIGHT(double hEIGHT) {
+		HEIGHT = hEIGHT;
+	}
+
+	public boolean isLava() {
+		return isLava;
+	}
+
+	public void setLava(boolean isLava) {
+		this.isLava = isLava;
+	}
+
+	public Color getDEFAULT_COLOR() {
+		return DEFAULT_COLOR;
+	}
+
+	public Color getLAVA_COLOR() {
+		return LAVA_COLOR;
 	}
 	
 }
