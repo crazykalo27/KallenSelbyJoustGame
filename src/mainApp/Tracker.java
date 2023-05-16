14c023e95b8f6c4b497d0909f83d238a363f6bac
Kallen Selby <crazykalo27@gmail.com>
2023-05-16 18:58:44 -0400

    removed bounding box

--- src/mainApp/Tracker.java
+++ src/mainApp/Tracker.java
@@ -27,14 +27,6 @@ public class Tracker extends Enemy{
 		
 		//this.setHasGravity(false);
 	}
-	public void drawOn(Graphics2D g2) {
-		g2.setColor(COL);
-		g2.translate(this.getXCent(), this.getYCent());
-		g2.fill(new Rectangle2D.Double(-this.getWidth()/2,-this.getHeight()/2,this.getWidth(),this.getHeight()));
-		g2.translate(-this.getXCent(), -this.getYCent());
-		
-		super.drawOn(g2);
-	}
 	
 	@Override
 	//TODO could make this not dependent on location so it has a constant speed
